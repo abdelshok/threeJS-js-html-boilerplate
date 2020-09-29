2a7e0b52d9cd1bcf278e4d466b5078e5a7bf3cd6
abdelshok <aas698@nyu.edu>
2020-09-29 02:39:39 -0400

    Remove unnecessary scripts from HTML file

--- public/scripts.js
+++ public/scripts.js
@@ -131,7 +131,7 @@ let loadingGraphicalSceneFinished = false;
  *
  */
 
-const splitItems = Splitting();
+// const splitItems = Splitting();
 
 // console.log('Split Items are', splitItems);
 
@@ -491,10 +491,10 @@ document.body.appendChild( renderer.domElement );
 
 // #controls not used anymore so we comment them out 
 
-let controls = new THREE.OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
-controls.dampingFactor = 0.25;
-controls.enableZoom = true;
+// let controls = new THREE.OrbitControls(camera, renderer.domElement);
+// controls.enableDamping = true;
+// controls.dampingFactor = 0.25;
+// controls.enableZoom = true;
 
 
 // Sets the stats at the top left of the page so that we can test the frame rate per second of the website as we develop it
